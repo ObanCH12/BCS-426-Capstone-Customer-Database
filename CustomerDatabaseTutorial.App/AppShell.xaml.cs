e3f9f7debd5cb93333578c3106cf4f0d370e1891
ObanCH12 <72536042+ObanCH12@users.noreply.github.com>
2022-05-14 15:08:53 -0400

    Added AppShell, move AuthenticationControl. Also included the Microsoft.UI to project.

---
+++ CustomerDatabaseTutorial.App/AppShell.xaml.cs
@@ -0,0 +1,182 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using CustomerDatabaseTutorial.App.Views;
+using System;
+using Windows.ApplicationModel.Core;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+namespace CustomerDatabaseTutorial.App
+{
+    /// <summary>
+    /// The "chrome" layer of the app that provides top-level navigation with
+    /// proper keyboarding navigation.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the AppShell, sets the static 'Current' reference,
+        /// adds callbacks for Back requests and changes in the SplitView's DisplayMode, and
+        /// provide the nav menu list with the data to display.
+        /// </summary>
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                NavView.SelectedItem = CustomerListMenuItem;
+            };
+
+            // Set up custom title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            // Set XAML element as a draggable region.
+            Window.Current.SetTitleBar(AppTitleBar);
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonForegroundColor = Colors.Black;
+            AppTitle.Text = Windows.ApplicationModel.Package.Current.DisplayName;
+        }
+
+        /// <summary>
+        /// Gets the navigation frame instance.
+        /// </summary>
+        public Frame AppFrame => frame;
+
+        /// <summary>
+        /// Default keyboard focus movement for any unhandled keyboarding
+        /// </summary>
+        private void AppShell_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            FocusNavigationDirection direction = FocusNavigationDirection.None;
+            switch (e.Key)
+            {
+                case VirtualKey.Left:
+                case VirtualKey.GamepadDPadLeft:
+                case VirtualKey.GamepadLeftThumbstickLeft:
+                case VirtualKey.NavigationLeft:
+                    direction = FocusNavigationDirection.Left;
+                    break;
+                case VirtualKey.Right:
+                case VirtualKey.GamepadDPadRight:
+                case VirtualKey.GamepadLeftThumbstickRight:
+                case VirtualKey.NavigationRight:
+                    direction = FocusNavigationDirection.Right;
+                    break;
+
+                case VirtualKey.Up:
+                case VirtualKey.GamepadDPadUp:
+                case VirtualKey.GamepadLeftThumbstickUp:
+                case VirtualKey.NavigationUp:
+                    direction = FocusNavigationDirection.Up;
+                    break;
+
+                case VirtualKey.Down:
+                case VirtualKey.GamepadDPadDown:
+                case VirtualKey.GamepadLeftThumbstickDown:
+                case VirtualKey.NavigationDown:
+                    direction = FocusNavigationDirection.Down;
+                    break;
+            }
+
+            if (direction != FocusNavigationDirection.None &&
+                FocusManager.FindNextFocusableElement(direction) is Control control)
+            {
+                control.Focus(FocusState.Keyboard);
+                e.Handled = true;
+            }
+        }
+
+        public readonly string CustomerListLabel = "Customer list";
+
+        public readonly string OrderListLabel = "Order list";
+
+        /// <summary>
+        /// Navigates to the page corresponding to the tapped item.
+        /// </summary>
+        private void NavigationView_ItemInvoked(muxc.NavigationView sender, muxc.NavigationViewItemInvokedEventArgs args)
+        {
+            var label = args.InvokedItem as string;
+            var pageType =
+                label == CustomerListLabel ? typeof(CustomerListPage) : null;
+            //args.IsSettingsInvoked ? typeof(SettingsPage) :
+            //label == OrderListLabel ? typeof(OrderListPage) : null;
+            if (pageType != null && pageType != AppFrame.CurrentSourcePageType)
+            {
+                AppFrame.Navigate(pageType);
+            }
+        }
+
+        /// <summary>
+        /// Ensures the nav menu reflects reality when navigation is triggered outside of
+        /// the nav menu buttons.
+        /// </summary>
+        private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                if (e.SourcePageType == typeof(CustomerListPage))
+                {
+                    NavView.SelectedItem = CustomerListMenuItem;
+                }
+                //else if (e.SourcePageType == typeof(OrderListPage))
+                //{
+                 //   NavView.SelectedItem = OrderListMenuItem;
+                //}
+                //else if (e.SourcePageType == typeof(SettingsPage))
+                //{
+                 //   NavView.SelectedItem = NavView.SettingsItem;
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the View Code button is clicked. Launches the repo on GitHub. 
+        /// </summary>
+        private async void ViewCodeNavPaneButton_Tapped(object sender, TappedRoutedEventArgs e) =>
+            await Launcher.LaunchUriAsync(new Uri(
+                "https://github.com/Microsoft/Windows-appsample-customers-orders-database"));
+
+        /// <summary>
+        /// Navigates the frame to the previous page.
+        /// </summary>
+        private void NavigationView_BackRequested(muxc.NavigationView sender, muxc.NavigationViewBackRequestedEventArgs args)
+        {
+            if (AppFrame.CanGoBack)
+            {
+                AppFrame.GoBack();
+            }
+        }
+    }
+}
